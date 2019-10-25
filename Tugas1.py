e381bd491d9f08c8acfa1885b38177258ace468c
sitihadijahjamili <56400200+sitihadijahjamili@users.noreply.github.com>
2019-10-25 21:36:42 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,11 @@
+# ini variabel & nilai
+
+namalengkap = "Siti Hadijah Jamili"
+namapanggilan = "Jamili"
+npm = 311910708
+tempatlahir = "Bekasi"
+umur = "19"
+telf = "0838 1546 8018"
+alamat = "Sukatani"
+
+print("Assalamualaikum. \n let me introduce my self. my name is",namalengkap,", but you can call me",namapanggilan,".my mpm is",npm,". I was botn in",tempatlahir,"and I am",umur,"year old. I am very glad if you want to invite my house in",alamat,"So, don't forget to call me before with the number",telf)
